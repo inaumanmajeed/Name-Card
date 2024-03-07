2aa943dba08532b79fbe9ffc77861c6f4fbc109f
inaumanmajeed <inaumanmajeed@gmail.com>
2024-03-07 17:58:36 +0500

    updates

---
+++ src/contacts.js
@@ -0,0 +1,26 @@
+const contacts = [
+    {
+      name: "Beyonce",
+      imgURL:
+        "https://t3.ftcdn.net/jpg/06/46/00/00/360_F_646000029_JTtTlIY4MgOhl6U36hjDFsRjzMkRw9d1.jpg",
+      phone: "+123 456 789",
+      email: "b@beyonce.com",
+    },
+    {
+      name: "Jack Bauer",
+      imgURL:
+        "https://static01.nyt.com/images/2015/11/03/fashion/03OTR-B/03OTR-B-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+      phone: "+987 654 321",
+      email: "jack@nowhere.com",
+    },
+    {
+      name: "Chuck Norris",
+      imgURL:
+        "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+      phone: "+918 372 574",
+      email: "gmail@chucknorris.com",
+    },
+  ];
+  
+  export default contacts;
+  
